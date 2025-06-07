3634f4b9055b07caa27f9b0c96361adc2f6b6dcf
DISOB <105884221+DISOB@users.noreply.github.com>
2025-06-07 22:18:50 +0300

    Update init.sql
    update colums from new metrics , add types

--- init.sql
+++ init.sql
@@ -4,9 +4,15 @@ DROP TABLE IF EXISTS chatbot_logs;
 
 CREATE TABLE chatbot_logs (
     id SERIAL PRIMARY KEY,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date DATE NOT NULL,
+    question_time TIME NOT NULL,
+    answer_time TIME NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    campus VARCHAR(50) NOT NULL,
+    education_level VARCHAR(50) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    subcategory VARCHAR(50),
     query TEXT NOT NULL,
     response TEXT NOT NULL,
-    satisfaction INTEGER CHECK (satisfaction IN (0, 1)),
-    error_category TEXT
+    satisfaction INTEGER CHECK (satisfaction IN (0, 1))
 );
