d6d01b27fff60fa85dcfc2bf4253d43c01de6adf
DISOB <105884221+DISOB@users.noreply.github.com>
2025-06-11 02:36:57 +0300

    Update __init__.py
    add some imports

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,5 +1,11 @@
 from .metrics import show_metrics
 from .standard import show_standard_view
 from .developer import show_developer_view
+from .error_analysis import show_error_analysis
 
-__all__ = ['show_metrics', 'show_standard_view', 'show_developer_view']
+__all__ = [
+    'show_metrics',
+    'show_standard_view',
+    'show_developer_view',
+    'show_error_analysis'
+]
